894f1efeddbb49e1aaefd61c3346e4c1d6d10b34
jeffreyvankampen <vankampenjeffrey@gmail.com>
2021-03-25 14:36:52 +0100

    drag and drop

--- Overzichtspagina/js/script.js
+++ Overzichtspagina/js/script.js
@@ -1,18 +1,77 @@
-const dragElementen = document.querySelector("#dragElement");
-const img =  document.querySelector('img')
+var dragged;
 
-dragElementen.addEventListener("dragstart", e => {
-  e.dataTransfer.setData("text/plain", dragElementen.id);
-});
+  /* events fired on the draggable target */
+  document.addEventListener("drag", function( event ) {
 
-const dropZones = document.querySelectorAll(".drop-zone")
+  }, false);
 
-for (const dropZone of dropZones) {
-  dropZone.addEventListener("dragover", e => {
-    e.preventDefault();
-    dropZone.classList.add("drop-zone--over");
-  });
-}
+  document.addEventListener("dragstart", function( event ) {
+      // store a ref. on the dragged elem
+      dragged = event.target;
+      // make it half transparent
+      event.target.style.opacity = .5;
+  }, false);
+
+  document.addEventListener("dragend", function( event ) {
+      // reset the transparency
+      event.target.style.opacity = "";
+  }, false);
+
+  /* events fired on the drop targets */
+  document.addEventListener("dragover", function( event ) {
+      // prevent default to allow drop
+      event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function( event ) {
+      // highlight potential drop target when the draggable element enters it
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "purple";
+      }
+
+  }, false);
+
+  document.addEventListener("dragleave", function( event ) {
+      // reset background of potential drop target when the draggable element leaves it
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "";
+      }
+
+  }, false);
+
+  document.addEventListener("drop", function( event ) {
+      // prevent default action (open as link for some elements)
+      event.preventDefault();
+      // move dragged elem to the selected drop target
+      if ( event.target.className == "dropzone" ) {
+          event.target.style.background = "";
+          dragged.parentNode.removeChild( dragged );
+          event.target.appendChild( dragged );
+      }
+    
+  }, false);
+
+
+// 1e versie
+
+// const dragElementen = document.querySelector("#dragElement");
+// const img =  document.querySelector('img')
+
+// dragElementen.addEventListener("dragstart", e => {
+//   e.dataTransfer.setData("text/plain", dragElementen.id);
+// });
+
+// const dropZones = document.querySelectorAll(".drop-zone")
+
+// for (const dropZone of dropZones) {
+//   dropZone.addEventListener("dragover", e => {
+//     e.preventDefault();
+//     dropZone.classList.add("drop-zone--over");
+//   });
+// }
+
+
+// 2e versie
 
 // const dragElementen = document.getElementById("dragElement");  
 // const img =  document.querySelector('img')
